f818f4e1df484a40a10ade234e1cbe6bb1aa2142
amakaohiku <amakanza1@gmail.com>
2020-12-07 05:36:40 -0700

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+numbers =[1, 2, 3, 4, 5, .100]
+for (var i= 0; i < 101; i++) {
+    if (numbers [i]% 2 === 0){
+        console.log('even');
+    }
+} for (var i= 0; i < 101; i++) {
+    if (numbers [i]% 2 !== 0){
+        console.log('odd');
+    
+    }
+};
